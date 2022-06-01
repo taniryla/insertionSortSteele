e612aaee027d45279458fd9bb910f85d2dba16ae
Ryland Taniguchi <ryland0000@gmail.com>
2022-05-31 21:19:53 -0700

    nested for loop

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 function insertionSort(arr) {
+  // start with the 2nd element
   let second = arr[i + 1];
+  // compare and swap
   if (second < arr[i]) {
     let temp = arr[i];
     arr[i] = arr[i + 1];
     arr[i + 1] = temp;
   }
+  for (let j = i + 2; j < arr.length; j++) {}
 }
