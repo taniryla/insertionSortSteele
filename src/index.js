54e90c7bd12630e49825ba007a607324ba54dda1
Ryland Taniguchi <ryland0000@gmail.com>
2022-05-31 21:01:54 -0700

    pick the 2nd element, compare and swap

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-function insertionSort(arr) {}
+function insertionSort(arr) {
+  let second = arr[i + 1];
+  if (second < arr[i]) {
+    let temp = arr[i];
+    arr[i] = arr[i + 1];
+    arr[i + 1] = temp;
+  }
+}
