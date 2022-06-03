bf6eada5e6a763875c362cad93ded12437119242
Ryland Taniguchi <ryland0000@gmail.com>
2022-06-02 23:48:38 -0700

    add to leftArr

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 function insertionSort(arr) {
   // created a sortedLeftArr
-  let sortedLeftArr = [];
+  let leftArr = [];
   // start with the 2nd element
   for (let i = 0; i < arr.length; i++) {
     let start = arr[i + 1];
@@ -9,20 +9,17 @@ function insertionSort(arr) {
       let temp = arr[i];
       arr[i] = arr[i + 1];
       arr[i + 1] = temp;
-      sortedLeftArr.unshift(arr[i + 1]);
+      leftArr.unshift(arr[i]);
+      leftArr.unshift(arr[i + 1]);
       arr.shift(arr[i + 1]);
     }
-    console.log(sortedLeftArr);
-    for (let j = i + 2; j < sortedLeftArr.length; j++) {
+    console.log(leftArr);
+    for (let j = i + 2; j < leftArr.length; j++) {
       if (arr[j] < arr[i + 1]) {
-        let temp1 = arr[i + 1];
-        arr[i + 1] = arr[j];
-        arr[j] = temp;
-        sortedLeftArr.push(arr[j]);
+        leftArr.unshift(arr[j]);
         arr.shift(arr[j]);
       }
+      // iterate through leftArr and sort into the right order
     }
   }
 }
-
-console.log(insertionSort([3, 6, 8, 2, 6, 5]));
