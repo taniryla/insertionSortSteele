5574a28f6f3daf6f0c8e521879599f899547099d
Ryland Taniguchi <ryland0000@gmail.com>
2022-06-02 22:35:17 -0700

    change variable name to start

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 function insertionSort(arr) {
   // start with the 2nd element
-  let second = arr[i + 1];
+  let start = arr[i + 1];
   // compare and swap
-  if (second < arr[i]) {
+  if (start < arr[i]) {
     let temp = arr[i];
-    arr[i] = arr[i + 1];
-    arr[i + 1] = temp;
+    arr[i] = start;
+    start = temp;
   }
   for (let j = i + 2; j < arr.length; j++) {}
 }
