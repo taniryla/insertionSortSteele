61ac71b49231d354b5f1c5733f52ed3a62773575
Ryland Taniguchi <ryland0000@gmail.com>
2022-06-02 23:38:03 -0700

    fixing logic

--- src/index.js
+++ src/index.js
@@ -2,24 +2,27 @@ function insertionSort(arr) {
   // created a sortedLeftArr
   let sortedLeftArr = [];
   // start with the 2nd element
-  let start = arr[i + 1];
-  // compare and swap
-  if (start < arr[i]) {
-    let temp = arr[i];
-    arr[i] = start;
-    start = temp;
-    sortedLeftArr.push(start);
-    arr.shift(start);
-  }
-  for (let j = i + 2; j < i + 1; j++) {
-    if (arr[j] < start) {
-      let temp1 = start;
-      start = arr[j];
-      arr[j] = temp;
-    } else if (arr[i] < start) {
-      let temp2 = start;
-      start = arr[i];
-      arr[i] = temp;
+  for (let i = 0; i < arr.length; i++) {
+    let start = arr[i + 1];
+    // compare and swap
+    if (arr[i + 1] < arr[i]) {
+      let temp = arr[i];
+      arr[i] = arr[i + 1];
+      arr[i + 1] = temp;
+      sortedLeftArr.unshift(arr[i + 1]);
+      arr.shift(arr[i + 1]);
+    }
+    console.log(sortedLeftArr);
+    for (let j = i + 2; j < sortedLeftArr.length; j++) {
+      if (arr[j] < arr[i + 1]) {
+        let temp1 = arr[i + 1];
+        arr[i + 1] = arr[j];
+        arr[j] = temp;
+        sortedLeftArr.push(arr[j]);
+        arr.shift(arr[j]);
+      }
     }
   }
 }
+
+console.log(insertionSort([3, 6, 8, 2, 6, 5]));
