e98ba1cb4b4183d26686edabce5163e54876ef6c
Ryland Taniguchi <ryland0000@gmail.com>
2022-06-03 00:57:59 -0700

    final code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 function insertionSort(arr) {
-  // created a sortedLeftArr
-  let leftArr = [];
   // start with the 2nd element
-  for (let i = 1; i < arr.length; i++) {
+  for (var i = 1; i < arr.length; i++) {
     // compare and swap
     let currentVal = arr[i];
-    for (let j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+    for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
       // iterate through leftArr and sort into the right order
-        arr[j+1] = arr[j];
-      }
-      // insert currentVal
-      arr[j+1] = currentVal;
+      arr[j + 1] = arr[j];
     }
+    // insert currentVal
+    arr[j + 1] = currentVal;
   }
   return arr;
 }
