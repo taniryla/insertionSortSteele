d2f4db5a37fa3f4baa97575d315a5fc06c8eb158
Ryland Taniguchi <ryland0000@gmail.com>
2022-06-02 23:17:11 -0700

    working through logic

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 function insertionSort(arr) {
+  // created a sortedLeftArr
+  let sortedLeftArr = [];
   // start with the 2nd element
   let start = arr[i + 1];
   // compare and swap
@@ -6,8 +8,18 @@ function insertionSort(arr) {
     let temp = arr[i];
     arr[i] = start;
     start = temp;
+    sortedLeftArr.push(start);
+    arr.shift(start);
   }
   for (let j = i + 2; j < i + 1; j++) {
-    if ()
+    if (arr[j] < start) {
+      let temp1 = start;
+      start = arr[j];
+      arr[j] = temp;
+    } else if (arr[i] < start) {
+      let temp2 = start;
+      start = arr[i];
+      arr[i] = temp;
+    }
   }
 }
