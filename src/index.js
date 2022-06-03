5539858d1dd0c3326106108f9faff4df27adc49d
Ryland Taniguchi <ryland0000@gmail.com>
2022-06-03 00:44:36 -0700

    reverse iteration for j

--- src/index.js
+++ src/index.js
@@ -2,29 +2,14 @@ function insertionSort(arr) {
   // created a sortedLeftArr
   let leftArr = [];
   // start with the 2nd element
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = 1; i < arr.length; i++) {
     // compare and swap
-    if (arr[i + 1] < arr[i]) {
-      let temp = arr[i];
-      arr[i] = arr[i + 1];
-      arr[i + 1] = temp;
-      leftArr.unshift(arr[i + 1]);
-      arr.shift(arr[i + 1]);
-    } else {
-      leftArr.unshift(arr[i]);
-      arr.shift(arr[i]);
-    }
-    console.log(leftArr, arr[i]);
-    for (let j = i + 1; j < leftArr.length; j++) {
-      if (arr[j] < arr[i]) {
-        let temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-        leftArr.unshift(arr[j]);
-        arr.shift(arr[j]);
-      } else {
-        leftArr.unshift(arr[i]);
-        arr.shift(arr[i]);
+    let currentVal = arr[i];
+    for (let j = i - 1; j >= 0; j--) {
+      if (arr[i] < arr[j]) {
+        let temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
       }
       // iterate through leftArr and sort into the right order
     }
